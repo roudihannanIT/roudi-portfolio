7674b3db32a606035e4804a9bade863b203f05e5
Roudi Hannan <roudihannan7@gmail.com>
2026-03-01 12:38:08 +0100

    chore: setup scable project folder structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
-      <h1 className="text-4xl font-bold">Roudi's Portfolio</h1>
+      <h1 className="text-4xl font-bold">Roudi&apos;s Portfolio</h1>
     </main>
   );
 }
