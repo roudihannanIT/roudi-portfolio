41dc7a55b45a085c6e6cff6701bab66fcb1884a6
Roudi Hannan <roudihannan7@gmail.com>
2026-03-01 15:11:04 +0100

    feat: add i18n Hero Section with responsive layout, language-aware CV download

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -2,13 +2,21 @@
 
 import Image from "next/image";
 import { FaGithub, FaLinkedin, FaEnvelope, FaInstagram, FaTelegram } from "react-icons/fa";
+import { useLocale, useTranslations } from "next-intl";
 
 export default function HeroSection() {
+
+  const t = useTranslations("Hero");
+
+  const locale = useLocale();
+
+  const cvFile = locale === "de" ? "/cv_de.pdf" : "/cv_en.pdf";
+
   return (
     <section className="w-full min-h-screen bg-linear-to-r from-blue-600 to-purple-600 flex items-center justify-center px-6">
       <div className="max-w-5xl w-full flex flex-col lg:flex-row items-center lg:items-start justify-between gap-12">
         {/* Left - Roudi's Image */}
-        <div className="flex-shrink-0">
+        <div className="shrink-0">
           <Image
             src="/roudiPic.webp"
             alt="Roudi Hannan"
@@ -24,27 +32,27 @@ export default function HeroSection() {
 
             {/* Texts */}
           <h1 className="text-4xl md:text-5xl font-bold text-white">
-            Hi, I’m Roudi Hannan
+            {t("greeting")} 
           </h1>
           <h2 className="text-xl md:text-2xl text-gray-200">
-            Software Engineer - Full-Stack Web Development
+            {t("title")}
           </h2>
 
           {/* CTA Buttons */}
           <div className="flex flex-col sm:flex-row gap-4 mt-4">
             <a
             // add cv in future
-              href="/cv.pdf"
+              href={cvFile}
               download
               className="px-6 py-3 bg-teal-500 text-white rounded-md font-medium text-sm hover:bg-teal-600 transition"
             >
-              Download CV
+              {t("downloadCV")}
             </a>
             <a
               href="#projects"
               className="px-6 py-3 border border-white text-white rounded-md font-medium text-sm hover:bg-white hover:text-gray-800 transition"
             >
-              View Projects
+              {t("viewProjects")}
             </a>
           </div>
 
